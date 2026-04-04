549e233cf816ceb158d18d43a71a8f58b548bd19
Raman <raman11ksharma@gmail.com>
2026-04-04 20:54:12 +0530

    LeetCode

---
+++ src/Leetcode/jAVA/RemoveDuplicates.java
@@ -0,0 +1,27 @@
+package Leetcode.jAVA;
+
+public class RemoveDuplicates {
+    public static int removeDuplicates(int[] nums){
+        if(nums.length==0){
+            return 0;
+        }
+        int index =1;
+        for(int i =1;i< nums.length;i++){
+            if(nums[i]!= nums[i-1]){
+                nums[index++]=nums[i];
+            }
+        }
+ return index;
+    }
+
+
+    public static void main (String[] args){
+        int [] nums = {0,0,1,1,1,2,2,3,3,4};
+        int length = removeDuplicates(nums);
+        System.out.println(length);
+        for(int i =0;i<length;i++){
+            System.out.print(nums[i]+" ");
+        }
+
+    }
+}
